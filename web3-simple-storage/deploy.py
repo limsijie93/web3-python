876004d6976b41d7263aedf481e96bf0d1d57c9a
sijielim <limsijie93@gmail.com>
2022-10-26 19:44:24 -0400

    init commit for simple storage

---
+++ web3-simple-storage/deploy.py
@@ -0,0 +1,27 @@
+import json
+
+from solcx import compile_standard
+
+solcx.install_solc('0.6.0')
+
+with open("./SimpleStorage.sol", "r") as f:
+    s2_file = f.read()
+    print(s2_file)
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleSotrage.sol": {"content": s2_file}},
+        "settings": {"outputSelection":
+            {"*":
+                {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"] # TODO: Find out what are these
+                }
+            }
+        }
+    },
+    solc_version="0.6.0"
+)
+
+with open("compiled_code.json", "w") as f:
+    json.dump(compiled_sol, f)
