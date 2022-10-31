787b11f03510e5302f7eb868fc171c380f6c1752
sijielim <limsijie93@gmail.com>
2022-10-31 19:22:58 -0400

    starting from 4 hour mark

--- web3-simple-storage/deploy.py
+++ web3-simple-storage/deploy.py
@@ -46,3 +46,37 @@ private_key = os.getenv("PRIVATE_KEY")
 # Create contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 print(SimpleStorage)
+
+# Build a transaction
+## To make a state change on the blockchain.
+
+####### Nounce
+nounce = w3.eth.getTransactionCount(my_addr)
+print(nounce)
+
+### 1. Build contract Deploy Transaction
+transaction = SimpleStorage.constructor().build_transaction({
+    "chainid": chain_id,
+    "from": my_addr,
+    "nounce": nounce
+})
+## This returns the data key. The value encompasses what happens in the SimpleStorage blockchain
+
+### 2. Sign the Transaction
+signed_txn = w3.eth.account.sign_transaction(
+    transaction=transaction, private_key=private_key)
+
+### 3. Send Transaction to the blockchaincal
+
+txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash) # this waits for transaction to go through
+
+## When working with the contract, you need 2 things
+## 1. Contract Address
+## 2. ABI
+simple_storage = w3.eth.contract(address=txn_receipt.contractAddress, abi=abi)
+
+## NOTE: We can't just use print. We need to add either Call or Transact method behind
+print(simple_storage.functions.retrieve().call(0)) # this is the rerieve function from the SimpleStorage.sol
+## Call: Doesn't make a state change to the blockchain
+## Transact: We make a state change
